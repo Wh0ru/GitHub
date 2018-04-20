b02d14dadf569b3daa2df3ceb284e10bf8d64b86
miro <2672339375@qq.com>
2018-04-20 16:20:29 +0800

    update

--- blogproject/comments/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Comment
-from django.contrib.auth import get_user_model
-
-class CommentForm(forms.ModelForm):
-    class Meta:
-        model=Comment
-        fields=['name','email','url','text']
